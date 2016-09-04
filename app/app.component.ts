f98d4cf25cf0d2dd459b60c29b6f56f981ea5e92
unknown <>
2016-09-04 23:07:53 +0800

    service and more than one components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,36 +1,12 @@
 import { Component } from '@angular/core';
-
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
-      <div *ngIf="selectedHero">
-          <h2>{{selectedHero.name}} details!</h2>
-          <div><label>id: </label>{{selectedHero.id}}</div>
-          <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name">
-          </div>
-      </div>
       <h2>My Heros</h2>
       <ul class="heroes">
           <li *ngFor="let hero of heroes"
@@ -39,6 +15,7 @@ const HEROES: Hero[] = [
               <span class="badge">{{hero.id}}</span>{{hero.name}}
           </li>
       </ul>
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
       `,
    styles:[`
       .selected {
@@ -88,15 +65,24 @@ const HEROES: Hero[] = [
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+    providers:[HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
     selectedHero: Hero;
-    heroes = HEROES;
-
+    heroes: Hero[];
+    constructor(private heroService: HeroService) {
+      
+    }
     onSelect(hero: Hero): void {
-        this.selectedHero = hero;
+      this.selectedHero = hero;
+    }
+    getHeroes():void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit():void {
+      this.getHeroes();
     }
 }
 

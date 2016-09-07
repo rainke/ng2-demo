04f19ee8733c5d68b2cef0d693084cdc11bad78f
hepeng <rainke@magicare.me>
2016-09-07 14:03:10 +0800

    detail

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
@@ -10,7 +11,10 @@ export class DashboardComponent implements OnInit{
 
     heroes:Hero[] = [];
 
-    constructor(private heroService: HeroService) { }
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) { }
 
     ngOnInit(): void {
         this.heroService.getHeroes()
@@ -18,6 +22,7 @@ export class DashboardComponent implements OnInit{
     }
 
     gotoDetail(hero: Hero):void {
-
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
     }
 }
